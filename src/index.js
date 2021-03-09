ca920b0d20e6a0313e9ddb3bc622c766a5c98d07
Maxim <kiber987@gmail.com>
2021-03-09 21:44:11 +0300

    feat: add functionality 0-999 numbers

--- src/index.js
+++ src/index.js
@@ -1,187 +1,164 @@
 module.exports = function toReadable (number) {
-//	toReadable(1); // Will return 'one'
-//	toReadable(997); //will return 'nine hundred ninety seven'
-//	let i = -192;
-//	let o;
-//	number = 987;
+
+//	number = 11;
 	o = Math.abs(number).toString().split('');//.reverse().join('');
+	newArray = [];
+	numLength = number.toString().length - 1;
+//	for (n = 0;  n < number.toString().length; n++){
+//	for (n = numLength; n >= 0; n--){
+//	console.log('numLength = ' + numLength);
+//	console.log('o[numLength] = ' + o[numLength]);
+//  console.log('o[numLength-1] = ' + o[numLength-1]);
 	
+	if (((+o[numLength]) == 0) && (numLength == 0)) // 0
+		newArray.unshift('zero');
+	if ((+o[numLength-1]) != 1) // 1-9
+		switch(+o[numLength]) {  
+			case 1:
+				newArray.unshift('one');
+				break;
+			case 2:
+				newArray.unshift('two');
+				break;
+			case 3:
+				newArray.unshift('three');
+				break;
+			case 4:
+				newArray.unshift('four');
+				break;
+			case 5:
+				newArray.unshift('five');
+				break;
+			case 6:
+				newArray.unshift('six');
+				break;
+			case 7:
+				newArray.unshift('seven');
+				break;				
+			case 8:
+				newArray.unshift('eight');
+				break;
+			case 9:
+				newArray.unshift('nine');
+				break;
+/*			case 0:
+				newArray.unshift('zero');
+				break;				
+*/
+//			default:
+//				newArray.unshift('***';
+//				break;
+		}
+
+  	else if ((+o[numLength-1]) == 1) // 10-19
+      	switch(+o[numLength]) {
+			case 0:
+				newArray.unshift('ten');
+				break;				
+			case 1:
+				newArray.unshift('eleven');
+				break;
+			case 2:
+				newArray.unshift('twelve');
+				break;
+			case 3:
+				newArray.unshift('thirteen');
+				break;
+			case 4:
+				newArray.unshift('fourteen');
+				break;
+			case 5:
+				newArray.unshift('fifteen');
+				break;
+			case 6:
+				newArray.unshift('sixteen');
+				break;
+			case 7:
+				newArray.unshift('seventeen');
+				break;				
+			case 8:
+				newArray.unshift('eighteen');
+				break;
+			case 9:
+				newArray.unshift('nineteen');
+				break;
+			default:
+				newArray.unshift('***');
+				break;
+			}
+
+	if (((+o[numLength-1]) >= 2)/* && (+o[numLength]) == 0 */) // 20-99
+      	switch(+o[numLength-1]) {
+			case 2:
+				newArray.unshift('twenty');
+				break;
+			case 3:
+				newArray.unshift('thirty');
+				break;
+			case 4:
+				newArray.unshift('forty');
+				break;
+			case 5:
+				newArray.unshift('fifty');
+				break;
+			case 6:
+				newArray.unshift('sixty');
+				break;
+			case 7:
+				newArray.unshift('seventy');
+				break;				
+			case 8:
+				newArray.unshift('eighty');
+				break;
+			case 9:
+				newArray.unshift('ninety');
+				break;
+			default:
+				newArray.unshift('***');
+				break;
+		}
+
+	if (((+o[numLength-2]) > 0)/* && (+o[numLength]) == 0 */) // 20-99
+//      newArray.unshift('twenty');
+      	switch(+o[numLength-2]) {
+			case 1:
+				newArray.unshift('one hundred');
+				break;
+			case 2:
+				newArray.unshift('two hundred');
+				break;
+			case 3:
+				newArray.unshift('three hundred');
+				break;
+			case 4:
+				newArray.unshift('four hundred');
+				break;
+			case 5:
+				newArray.unshift('five hundred');
+				break;
+			case 6:
+				newArray.unshift('six hundred');
+				break;
+			case 7:
+				newArray.unshift('seven hundred');
+				break;				
+			case 8:
+				newArray.unshift('eight hundred');
+				break;
+			case 9:
+				newArray.unshift('nine hundred');
+				break;
+			}
+
+	return(newArray.join(' '));
+}
+
+//console.log(number + ' = ' + newArray.join(' '));
+//console.log(o);
 //	number.toString().length - 1 единицы
 //	number.toString().length - 2 десятки
 //	number.toString().length - 3 сотни
-
 //	function count1
 //	function count10
 //	function count100
-
- 	
-	
-	for (n = 0;  n < number.toString().length; n++){
-//		console.log(o[n]);
-  		if (n == 2)
-			switch(+o[n]) {
-			case 1:
-				o[n] = 'one';
-				break;
-			case 2:
-				o[n] = 'two';
-				break;
-			case 3:
-				o[n] = 'three';
-				break;
-			case 4:
-				o[n] = 'four';
-				break;
-			case 5:
-				o[n] = 'five';
-				break;
-			case 6:
-				o[n] = 'six';
-				break;
-			case 7:
-				o[n] = 'seven';
-				break;				
-			case 8:
-				o[n] = 'eight';
-				break;
-			case 9:
-				o[n] = 'nine';
-				break;
-			case 0:
-				o[n] = 'zero';
-				o.length--;
-				break;				
-//			default:
-//				o[n] = '***';
-//				break;
-		}
-	if (n == 1)
-			switch(+o[n]) {
-			case 1:
-//				o[n] = '*one*';
-	
-				switch(+o[n+1]) {
-				case 1:
-					o[n] = 'eleven';
-					o.length--;
-					break;
-				case 2:
-					o[n] = 'twelve';
-					o.length--;
-					break;
-				case 3:
-					o[n] = 'thirteen';
-					o.length--;
-					break;
-				case 4:
-					o[n] = 'fourteen';
-					o.length--;
-					break;
-				case 5:
-					o[n] = 'fifteen';
-					o.length--;
-					break;
-				case 6:
-					o[n] = 'sixteen';
-					o.length--;
-					break;
-				case 7:
-					o[n] = 'seventeen';
-					o.length--;
-					break;				
-				case 8:
-					o[n] = 'eighteen';
-					o.length--;
-					break;
-				case 9:
-					o[n] = 'nineteen';
-					o.length--;
-					break;
-				case 0:
-					o[n] = '*zero*';
-					break;				
-				default:
-					o[n] = '***';
-					break;
-			}
-	
-				break;
-			case 2:
-				o[n] = 'twenty';
-				break;
-			case 3:
-				o[n] = 'thirty';
-				break;
-			case 4:
-				o[n] = 'forty';
-				break;
-			case 5:
-				o[n] = 'fifty';
-				break;
-			case 6:
-				o[n] = 'sixty';
-				break;
-			case 7:
-				o[n] = 'seventy';
-				break;				
-			case 8:
-				o[n] = 'eighty';
-				break;
-			case 9:
-				o[n] = 'ninety';
-				break;
-			case 0:
-//				o[n] = '*zero*';
-				o[n] = o[n+1];
-				o.length--;
-				break;				
-			default:
-				o[n] = '***';
-				break;
-		}
-		if (n == 0)
-			switch(+o[n]) {
-			case 1:
-				o[n] = 'one hundred';
-				break;
-			case 2:
-				o[n] = 'two hundred';
-				break;
-			case 3:
-				o[n] = 'three hundred';
-				break;
-			case 4:
-				o[n] = 'four hundred';
-				break;
-			case 5:
-				o[n] = 'five hundred';
-				break;
-			case 6:
-				o[n] = 'six hundred';
-				break;
-			case 7:
-				o[n] = 'seven hundred';
-				break;				
-			case 8:
-				o[n] = 'eight hundred';
-				break;
-			case 9:
-				o[n] = 'nine hundred';
-				break;
-			case 0:
-				o[n] = '*zero*';
-				break;				
-			default:
-				o[n] = '***';
-				break;
-		}
-
-	}
-//console.log(o.join(' '));
-//console.log(number.toString().length);
-
-	return o.join(' ');
-	
-//console.log(o);
-//console.log(typeof(o));
-}
+// else if (
